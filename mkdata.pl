91a69864efd59354afa246462a724e439673d23a
Keith J Baugh <keith.baugh@hays.com>
2020-10-01 16:20:40 +0100

    Book data added..

---
+++ mkdata.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+open(IN, "ttbooks.raw") or die;
+
+my ($auth, $isbn, $title);
+
+while (<IN>){
+   chomp;
+   $author = $1 if m/Autho.*: (.*)/;   
+   $isbn = $1 if m/ISBN-13: (.*)/;   
+   if (m/View This Book/){
+      $title = <IN>;
+      chomp($title);
+      $title =~ s/'/''/g;
+      $author =~ s/'/''/g;
+      print "INSERT INTO books (book_id, isbn, author) VALUES (book_id_seq.nextval, '$isbn','$author');\n";
+      print "INSERT INTO book_info(book_id, lang_id, sale_status, translation_status_id, uv_title, uv_description)\n";
+      print "VALUES(book_id_seq.curval, 1, 'ONSALE', 1, '$title', 'Description for book with title: $title');\n";
+   }
+}
+
+
+
+
+__DATA__
+                                                                                                                                                                                                                                                                                                                                                                                    
+
+Author: Botma, Grant
+ISBN-13: 9781544505411
+ISBN-10: 1544505418
+View This Book ›
+Best Practices in Talent Management: How the World's Leading Corporations Manage, Develop, and Retain Top Talent
+Best Practices in Talent Management: How the World's Leading Corporations Manage, Develop, and Retain Top Talent
+
